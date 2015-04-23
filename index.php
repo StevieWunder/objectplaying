91409120ac1e43ab01704103002c4922f9e18554
Stevie Wunder <sprein@gmail.com>
2015-04-23 15:28:13 +0200

    index uitgebreid door asiel aante maken en wat spelen met properties

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-    echo '<a href="forms/adddog.html">Hond toevoegen</a>';
+    include 'classes/model_building.php';
+    include 'classes/model_shelter.php';
+    echo '<a href="forms/adddog.html">Hond toevoegen</a><P>';
+    $hondenAsiel = new Model_Shelter('The happy dog',1, NULL, 3, 4.5, 8, 'Barkstreet', 12, '12345', 'DogVille');
+    echo 'De gegevens van hondenasiel met naam \'' . $hondenAsiel->name . '\' zijn:<BR>';
+    echo '-------------------------------------------------------------------------<BR>';
+    echo '<HR>';
+    echo 'Het adres is ' . $hondenAsiel->street . ' ' . $hondenAsiel->streetNumber . ', ' . $hondenAsiel->postalCode . ' ' . $hondenAsiel->city . '.<BR>';
+    echo $hondenAsiel->adoptieMogelijk . '.<BR>';
+    echo $hondenAsiel->opvangMogelijk . '.<BR>';
+    echo 'De inhoud van het hondenAsiel is:' . $hondenAsiel->getVolume() . ' kuub.<BR>';
 ?>
