40c999ff1f27627c54ff63aa48a31a52385265c6
Stevie Wunder <sprein@gmail.com>
2015-04-23 15:24:27 +0200

    class gebouw om te extenden naar bijvoorbeeld een asiel

---
+++ classes/model_building.php
@@ -0,0 +1,10 @@
+<?php
+class Model_Building{
+    protected $width;
+    protected $length;
+    protected $height;
+    public $city;
+    public $postalCode;
+    public $street;
+    public $streetNumber;
+}
