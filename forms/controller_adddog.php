ed86776c0ee54be08c028548d878e61708dff02c
Stevie Wunder <sprein@gmail.com>
2015-04-23 15:27:28 +0200

    controller nu met klein beetje extra uitleg en mogelijkheid tot inserten hond en controle dat hond bestaat

--- forms/controller_adddog.php
+++ forms/controller_adddog.php
@@ -14,9 +14,6 @@
     $dogName =$_POST['name'];
     $dogRace =$_POST['race'];
 
-
-
-
     /*
      *
      * Hier eerst checken of deze waardes wel bestaan.
@@ -24,33 +21,36 @@
            dan pas inserten. nders gaat het mis.
         }
 
-        Valideren van user input is een heel belangrijk iets in programmeren.
-        Je moet te allen tijde controleren of er waardes zijn en of ze van het type zijn dat je verwacht.
-        Dit ook / vooral ivm veiligheid.
+
 
      *
      *
      */
 
-    if (!empty($dogName) &&!empty($dogRace)){
+    if (!empty($dogName) &&!empty($dogRace)) {
 
         //De gebruiker heeft via een form een naam en een ras gestuurd. We  gaan op zoek naar een hond die hierbij past:
 
-        $hondje = model_dog::forge($dogName,$dogRace);
+        $vindHondje = model_dog::forge($dogName, $dogRace);
 
         // Het kan zijn dat deze hond 'null' is, check model dog regel 55. IN dat geval hebben we niet zo'n hond.
-
         // Hier checken we op:
 
-        if (empty($hondje)) {
+        // SP: En in het geval we niet zo'n hond hebben gaan we dus inserten, want hij bestaat nog niet.
 
-            $hondje = new Model_Dog($dogName,$dogRace);
+        if (empty($vindHondje)) {
+            $hondje = new Model_Dog($dogName, $dogRace);
+            //Onderstaande comment van TOM even laten staan: Is nu niet van belang omdat we alleen willen saven als het gaat om een hond die nog niet bestaat in dit voorbeeld.
+            //In een ander voorbeeld zullen we kijken dat we de hond gaan updaten indien deze is gevonden.
+
+            // TOM: Nu weten we zeker dat we een hond hebben, of uit de database of een nieuwe, we kunnen nu opslaan.
+            $hondje->save();
+        } else {
+            echo 'Deze dog bestaat al en kan niet 2 keer bestaan.<BR>';
+            echo 'Klik op deze link om opniew een hondje toe te voegen: ';
+            echo '<a href="../forms/adddog.html">Hond toevoegen</a>';
         }
-
-        // Nu weten we zeker dat we een hond hebben, of uit de database of een nieuwe, we kunnen nu opslaan.
-        $hondje->save();
-    }
-    else{
+    }else{
         echo 'Niet alle waarden zijn ingevuld.<BR>';
         echo 'Klik op deze link om opniew een hondje toe te voegen: ';
         echo '<a href="../forms/adddog.html">Hond toevoegen</a>';
